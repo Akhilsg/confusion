9c8b76ca303a37b4d03415df1ea9e48c74d82a6f
Akhil Gupta <gupta.akhil076@gmail.com>
2022-07-22 08:36:13 -0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,9 +4,11 @@ import {
   CardImg,
   CardText,
   CardBody,
-  CardTitle
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
 } from 'reactstrap';
-
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
   if (dish != null) {
@@ -64,8 +66,18 @@ const DishDetails = (props) => {
     return (
       <div className="container">
         <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.dish.comments} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     )
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,16 +3,21 @@ import {
 	Card,
 	CardImg,
 	CardImgOverlay,
-	CardTitle
+	CardTitle,
+	Breadcrumb,
+	BreadcrumbItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick }) {
 	return (
-		<Card onClick={() => onClick(dish.id)}>
-			<CardImg width="100%" src={dish.image} alt={dish.name} />
-			<CardImgOverlay>
-				<CardTitle>{dish.name}</CardTitle>
-			</CardImgOverlay>
+		<Card>
+			<Link to={`/menu/${dish.id}`}>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+				<CardImgOverlay>
+					<CardTitle>{dish.name}</CardTitle>
+				</CardImgOverlay>
+			</Link>
 		</Card>
 	);
 }
@@ -21,13 +26,23 @@ const Menu = (props) => {
 	const menu = props.dishes.map((dish) => {
 		return (
 			<div key={dish.id} className="col-12 col-md-5 m-1">
-				<RenderMenuItem dish={dish} onClick={props.onClick} />
+				<RenderMenuItem dish={dish} />
 			</div>
 		);
 	});
 
 	return (
 		<div className="container">
+			<div className="row">
+				<Breadcrumb>
+					<BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+					<BreadcrumbItem active>Menu</BreadcrumbItem>
+				</Breadcrumb>
+			</div>
+			<div className="col-12">
+				<h3>Menu</h3>
+				<hr />
+			</div>
 			<div className="row">
 				{menu}
 			</div>
